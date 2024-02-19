350fd3f3abf7ce6d2df38f251ddefe148ff9d9ff
billy <billy.xiezhimin@gmail.com>
2024-02-19 12:57:51 +0800

    init

---
+++ src/main/java/com/yuzhi/opsvr/opsvr/OpsvrApplication.java
@@ -0,0 +1,13 @@
+package com.yuzhi.opsvr.opsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpsvrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuzhi/opsvr/opsvr/OpsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzhi.opsvr.opsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
